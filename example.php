ebce1855c0ca9b26aa65d297bac13996a85e818a
Marek Ziółkowski <ziol3k@gmail.com>
2014-02-20 13:49:03 +0100

    Update example.php

--- example.php
+++ example.php
@@ -10,10 +10,11 @@ $states = array(
     'New', 'In progress'
 );
 $report = new \TpReport\Request('http://localhost/TargetProcess/api/v1/', 'YOUR_USERNAME', 'YOUR_PASSWORD');
+
+// Querying for list of new (state: 'New') bugs in projects defined in $projects
 $q = $report->collection('Bugs')
         ->where(array(
     'and',
-    "Priority.Name eq 'Unspecified'",
     array('in', 'EntityState.Name', $states),
     array('in', 'Project.Id', $projects)
         ));
