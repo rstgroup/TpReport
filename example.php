d6c7c67d15accb82e24a910c6d7624f9faa9b864
Marek Ziółkowski <ziol3k@gmail.com>
2014-02-20 16:02:18 +0100

    Update example.php

--- example.php
+++ example.php
@@ -5,17 +5,13 @@ require 'vendor/autoload.php';
 // IDs of projects
 $projects = array(1, 2, 4);
 
-// bugs states
-$states = array(
-    'New', 'In progress'
-);
 $report = new \TpReport\Request('http://localhost/TargetProcess/api/v1/', 'YOUR_USERNAME', 'YOUR_PASSWORD');
 
 // Querying for list of new (state: 'New') bugs in projects defined in $projects
 $q = $report->collection('Bugs')
         ->where(array(
     'and',
-    array('in', 'EntityState.Name', $states),
+    'EntityState.Name = New',
     array('in', 'Project.Id', $projects)
         ));
 
